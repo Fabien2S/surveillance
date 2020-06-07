a39ea435586921e9a2881c3f193555baf14037ab
DoubleDashes <18100602+DoubleDashes@users.noreply.github.com>
2020-06-07 18:36:14 +0200

    Fixed SteamGameAchievementModel.cs

--- Surveillance.Steam/Models/SteamGameAchievementModel.cs
+++ Surveillance.Steam/Models/SteamGameAchievementModel.cs
@@ -3,6 +3,6 @@
     public struct SteamGameAchievementModel
     {
         public string Name { get; set; }
-        public bool Achieved { get; set; }
+        public int Achieved { get; set; }
     }
 }
